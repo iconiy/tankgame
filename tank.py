26fa453b8defd9fbe3adbfa43e7ca4e8c57bc2da
iconiy <greg91moore@gmail.com>
2025-03-05 07:50:10 -0800

    Setup basic tank

---
+++ tank.py
@@ -0,0 +1,11 @@
+class Tank:
+
+    def __init__(self, health, attack, range, armor):
+        self.health = health
+        self.attack = attack
+        self.range = range
+        self.armor = armor
+
+    def attack():
+        space =+ 1
+
