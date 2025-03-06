b1a4fcb87d1a442cad2941924bae9bdf2563bbfe
iconiy <greg91moore@gmail.com>
2025-03-06 04:52:39 -0800

    Setup basic tank functions

--- tank.py
+++ tank.py
@@ -1,11 +1,25 @@
 class Tank:
 
-    def __init__(self, health, attack, range, armor):
+    def __init__(self, health, attack, range, armor, movement):
         self.health = health
         self.attack = attack
         self.range = range
         self.armor = armor
+        self.movement = movement
 
-    def attack():
-        space =+ 1
+    def attack(self):
+        pass
+
+    def siege(self):
+        Tank.range =+ 1
+        Tank.attack =+ 5
+        Tank.movement = 0
+        print('The tank has now in siege mode! ')
+
+    def hull_break(self):
+        Tank.armor =- 2
+        Tank.movement = 0
+        print('The tank has a hull breach and is under assault! ')
+
+
 
