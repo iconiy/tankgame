3fbb3e4a3e8c602c78c65d64a989f396e64a90fd
iconiy <greg91moore@gmail.com>
2025-03-17 10:35:16 -0700

    Setup attack method

--- tank.py
+++ tank.py
@@ -1,14 +1,15 @@
-import sys
+import random
 
 
 class Tank:
 
     def __init__(self, name):
         self.health = 150
-        self.attack = 25
+        self.attck = 25
         self.range = 12
         self.armor = 10
         self.movement = 2
+        self.ammo = 2
         self.name = name
 
     def death(self):
@@ -19,14 +20,16 @@ class Tank:
         return death
 
     def attack(self):
-        pass
+        print('BOOM!')
+        att = (int(self.attck) - int(random.randint(0, self.attck)))
+        print(f'You did {att} damage')
 
     def siege(self):
         self.range += 2
-        self.attack += 5
+        self.attck += 5
         self.movement = 0
         print('The tank has now in siege mode! ')
-        print(self.range, self.attack, self.movement)
+        print(self.range, self.attck, self.movement)
 
     def hull_break(self):
         Tank.armor =- 2
@@ -34,7 +37,7 @@ class Tank:
         print('The tank has a hull breach and is under assault! ')
 
     def stats(self):
-        print(f'The current stats of {self.name} is {self.health} health, and {self.attack} attack')
+        print(f'The current stats of {self.name} is {self.health} health, and {self.attck} attack')
 
 
 
